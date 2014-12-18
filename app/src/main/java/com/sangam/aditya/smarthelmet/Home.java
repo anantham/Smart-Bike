78c622854ef42c937bf7186311768da859a4f0a9
Aditya Prasad <adityaprasadiskool@gmail.com>
2014-12-18 22:06:57 +0530

    Make Button to set emergency numbers
    I made a button in the main - Home activity which asked the user to set
    his emergency numbers in red color text. If the numebers are already
    set then the text color will be green and the promt will be change the
    already set number.
    
    Signed-off-by: Aditya Prasad <adityaprasadiskool@gmail.com>

--- app/src/main/java/com/sangam/aditya/smarthelmet/Home.java
+++ app/src/main/java/com/sangam/aditya/smarthelmet/Home.java
@@ -1,17 +1,57 @@
 package com.sangam.aditya.smarthelmet;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 
 public class Home extends ActionBarActivity {
+    //CONSTANTS
+
+    // this is the String used to identify and accesses the shared preferences file used to store the numbers.
+    public static final String EMERGENCY_NUMBER = "com.sangam.smarthelmet_emergencynumbermasterkey";
+    public static final String EMERGENCY_NUMBER1 = "com.sangam.smarthelmet_emergencynumber1key";
+    public static final String EMERGENCY_NUMBER2 = "com.sangam.smarthelmet_emergencynumber2key";
+    public static final String EMERGENCY_NUMBER3 = "com.sangam.smarthelmet_emergencynumber3key";
+
+    // this is the default number which will be stored as emergency numbers
+    private static final long DEFAULT_EMERGENCY_NUMBER = 0;
+
+
+    // these are the variables which hold the numbers them self
+    long emergency_number1;
+    long emergency_number2;
+    long emergency_number3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        // We get the stored emergency numbers onto application's variables
+        restore_data();
+    }
+
+    private void restore_data() {
+        // here we get a handle on the shared preferences
+        SharedPreferences sharedPref = this.getSharedPreferences(EMERGENCY_NUMBER, Context.MODE_PRIVATE);
+        // Here we get the shared preferences values into usable variables
+        emergency_number1=sharedPref.getLong(EMERGENCY_NUMBER1, DEFAULT_EMERGENCY_NUMBER);
+        emergency_number2=sharedPref.getLong(EMERGENCY_NUMBER2, DEFAULT_EMERGENCY_NUMBER);
+        emergency_number3=sharedPref.getLong(EMERGENCY_NUMBER3, DEFAULT_EMERGENCY_NUMBER);
+
+        // Now if at least one such number has been saved then
+        if(emergency_number1!=DEFAULT_EMERGENCY_NUMBER || emergency_number2!=DEFAULT_EMERGENCY_NUMBER || emergency_number3!=DEFAULT_EMERGENCY_NUMBER) {
+            //change color of button which is used to Set new emergency numbers AND change its text
+            Button emergency = (Button) findViewById(R.id.button_emergency);
+            emergency.setTextColor(Color.GREEN);
+            emergency.setText("Change Emergency Numbers");
+        }
     }
 
 
