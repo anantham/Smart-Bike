6a3e78757a086471e2e18cfcefa64e1d401d921e
Aditya Prasad <adityaprasadiskool@gmail.com>
2014-12-20 14:11:20 +0530

    Add validation functionality before saving emergency numbers
    I have added the libphonenumber library as a java module to this project
    I am using this to validate the entered string before parsing it to long
    I have kept the main conditions as only integers and 10 numbers only.
    
    Signed-off-by: Aditya Prasad <adityaprasadiskool@gmail.com>

--- app/src/main/java/com/sangam/aditya/smarthelmet/EditEmergencyNumber.java
+++ app/src/main/java/com/sangam/aditya/smarthelmet/EditEmergencyNumber.java
@@ -10,6 +10,12 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.i18n.phonenumbers.NumberParseException;
+import com.google.i18n.phonenumbers.PhoneNumberUtil;
+import com.google.i18n.phonenumbers.Phonenumber;
+
+import java.util.regex.Pattern;
+
 
 public class EditEmergencyNumber extends ActionBarActivity {
 
@@ -45,11 +51,16 @@ public class EditEmergencyNumber extends ActionBarActivity {
         SharedPreferences pref = getApplicationContext().getSharedPreferences(Home.EMERGENCY_NUMBER_MASTER, MODE_PRIVATE);
         SharedPreferences.Editor editor = pref.edit();
 
+
         // Get the numbers which we need to store.
         EditText no1 = (EditText)findViewById(R.id.editText);
         EditText no2 = (EditText)findViewById(R.id.editText2);
         EditText no3 = (EditText)findViewById(R.id.editText3);
 
+        // Validate the entered numbers
+        if(!valid_numbers(no1.getText().toString(),no2.getText().toString(),no3.getText().toString())){
+            return;
+        }
 
         // Store these numbers into memory
         editor.putLong(Home.EMERGENCY_NUMBER1, Long.valueOf(no1.getText().toString()));
@@ -58,10 +69,69 @@ public class EditEmergencyNumber extends ActionBarActivity {
 
 
         // Save the changes in SharedPreferences ie commit changes
-        editor.commit();
+        editor.apply();
 
         // Inform the user the numbers have been changed
-        Toast.makeText(this, "Your Numbers have been saved", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Your Numbers have been Validated and Saved", Toast.LENGTH_SHORT).show();
+    }
+
+    private boolean valid_numbers(String no1,String no2,String no3) {
+        //check if the entered numbers are valid
+        if(no1.length()!=10 && !Pattern.matches("[a-zA-Z]+", no1)){
+            Toast.makeText(this, "Enter a 10 digit phone number (first entry)", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        else if(no2.length()!=10 && !Pattern.matches("[a-zA-Z]+", no2)){
+            Toast.makeText(this, "Enter a 10 digit phone number (second entry)", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        else if(no3.length()!=10 && !Pattern.matches("[a-zA-Z]+", no3)){
+            Toast.makeText(this, "Enter a 10 digit phone number (third entry)", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        PhoneNumberUtil phoneUtil = PhoneNumberUtil.getInstance();
+        Phonenumber.PhoneNumber num1 = null;
+        Phonenumber.PhoneNumber num2 = null;
+        Phonenumber.PhoneNumber num3 = null;
+
+
+        try {
+
+            // I set the default region to IN (India)
+            // You can find your country code here http://www.iso.org/iso/country_names_and_code_elements
+            num1 = phoneUtil.parse(no1,"IN");
+            num2 = phoneUtil.parse(no2,"IN");
+            num3 = phoneUtil.parse(no3,"IN");
+
+
+        } catch (NumberParseException e) {
+            // if there’s any error
+            Log.i("debug", "NumberParseException was thrown: " + e.toString());
+        }
+
+        // check if the the first number is valid
+        boolean isValid1 = phoneUtil.isValidNumber(num1);
+        // check if the second number is valid
+        boolean isValid2 = phoneUtil.isValidNumber(num2);
+        // check if the third number is valid
+        boolean isValid3 = phoneUtil.isValidNumber(num3);
+
+        // get the 3 number’s international format - NOT USED NOW
+        //String internationalFormat1 = phoneUtil.format(num1,PhoneNumberUtil.PhoneNumberFormat.INTERNATIONAL);
+        //String internationalFormat2 = phoneUtil.format(num2,PhoneNumberUtil.PhoneNumberFormat.INTERNATIONAL);
+        //String internationalFormat3 = phoneUtil.format(num3,PhoneNumberUtil.PhoneNumberFormat.INTERNATIONAL);
+
+
+       if(isValid1 && isValid2 && isValid3){
+           return true;
+       }
+       else{
+            // prompt the user when the number is invalid
+            Toast.makeText(getBaseContext(),"Phone numbers validation status - "+ isValid1+" "+isValid2+" "+isValid3,Toast.LENGTH_SHORT).show();
+            return false;
+       }
+
     }
 
 
